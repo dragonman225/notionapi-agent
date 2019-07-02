00afd57eefd86e8a90f58f05cb5086846ee3d48b
dragonman225 <zxcvb22217@gmail.com>
2019-07-03 02:40:53 +0800

    :art: New interface with more info. make-request.js is now an individual module. Remove console.log().

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = { log }
-
-/**
- * Wrapper of console.log().
- */
-function log() {
-  console.log.apply(null, arguments)
-}
---
+++ test/agent.spec.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const path = require('path')
+const NotionAgent = require('../lib/agent')
+
+/* Fill in your cookie. */
+const options = {
+  cookie: ''
+}
+
+const agent = new NotionAgent(options)
+
+async function main() {
+  try {
+    /* Fill in a page id. */
+    let pageId = '181e961a-eb5c-4ee6-9153-07c0dfd5156d'
+
+    console.log('Calling loadPageChunk')
+    let page = await agent.loadPageChunk(pageId)
+    console.log(`Remote response ${page.statusCode}`)
+
+    console.log('Calling getAssetsJson')
+    let assets = await agent.getAssetsJson()
+    console.log(`Remote response ${assets.statusCode}`)
+
+    console.log('Calling loadUserContent')
+    let userContent = await agent.loadUserContent()
+    console.log(`Remote response ${userContent.statusCode}`)
+
+    /** Save response data. */
+    let pageChunkFile = path.join(__dirname, 'PageChunk.json')
+    let assetsFile = path.join(__dirname, 'Assets.json')
+    let userContentFile = path.join(__dirname, 'UserContent.json')
+    fs.writeFileSync(pageChunkFile, JSON.stringify(page.data), { encoding: 'utf-8' })
+    fs.writeFileSync(assetsFile, JSON.stringify(assets.data), { encoding: 'utf-8' })
+    fs.writeFileSync(userContentFile, JSON.stringify(userContent.data), { encoding: 'utf-8' })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
