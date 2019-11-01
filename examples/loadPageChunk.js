80a5a5987dc68c7cddde8d1739a4454feeb57006
dragonman225 <zxcvb22217@gmail.com>
2019-11-01 19:58:15 +0800

    fix: Change pageId to correct one in example

--- examples/loadPageChunk.js
+++ examples/loadPageChunk.js
@@ -4,7 +4,7 @@ const agent = new NotionAgent()
 
 async function main() {
 
-  const pageId = '181e961a-eb5c-4ee6-9153-07c0dfd5156d0'
+  const pageId = '181e961a-eb5c-4ee6-9153-07c0dfd5156d'
 
   const page = await agent.loadPageChunk(pageId)
   if (page.error) {
