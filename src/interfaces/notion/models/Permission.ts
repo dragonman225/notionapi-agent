85e8a75ce2ada1e6c01943436e68355bfa30f62b
dragonman225 <zxcvb22217@gmail.com>
2019-12-17 03:44:16 +0800

    refactor: Notion API typings
    Correct permission models

--- src/interfaces/notion/models/Permission.ts
+++ src/interfaces/notion/models/Permission.ts
@@ -1,20 +1,29 @@
 import { UUID } from "./common"
 
 /** 
- * Role of an entity (for now, only human user).
+ * Role of an entity.
  */
 export type EntityRole =
   "editor" | "reader" | "none"
 
 /** 
- * Describe permission of an entity (for now, only human user).
+ * Describe permission of an public entity to access a page.
  */
-export interface Permission {
-  /** I have seen only `user_permission`. */
-  type: "user_permission" | string
-  user_id: UUID
+export interface PagePermission {
+  type: "public_permission"
   /** The entity's role. */
   role: EntityRole
   allow_duplicate?: boolean
   allow_search_engine_indexing?: boolean
+}
+
+/** 
+ * Describe permission of a Notion user to access a space.
+ */
+export interface SpacePermission {
+  type: "user_permission"
+  /** The Notion user's role. */
+  role: EntityRole
+  /** The Notion user's ID. */
+  user_id: UUID
 }
--- src/interfaces/notion/models/space/index.ts
+++ src/interfaces/notion/models/space/index.ts
@@ -1,5 +1,5 @@
 import { UUID, Timestamp, ModelName } from "../common"
-import { Permission } from "../Permission"
+import { SpacePermission } from "../Permission"
 
 /**
  * Describe a workspace, which can be thought of as a special case of 
@@ -9,7 +9,7 @@ export interface Space {
   id: UUID
   version: number
   name: string
-  permissions: Permission[]
+  permissions: SpacePermission[]
   beta_enabled: boolean
   pages: UUID[]
   created_by: UUID
