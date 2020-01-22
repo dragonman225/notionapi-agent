29c762e26425a2935a9a57db0fe05b0ca65fbd13
dragonman225 <zxcvb22217@gmail.com>
2020-01-22 17:15:05 +0800

    chore: Update user agent

--- src/strings.ts
+++ src/strings.ts
@@ -3,7 +3,7 @@
  */
 const Default = {
   server: "https://www.notion.so",
-  userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
 }
 
 export { Default }
